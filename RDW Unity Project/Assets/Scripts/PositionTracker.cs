b08b53d3798ca5b8cabd3a8a2394252ca3fbc847
Anthony Moore <68195709+antleemoore@users.noreply.github.com>
2024-04-24 08:39:20 -0400

    fix blue border issue

--- RDW Unity Project/Assets/Scripts/PositionTracker.cs
+++ RDW Unity Project/Assets/Scripts/PositionTracker.cs
@@ -60,7 +60,7 @@ public class PositionTracker : MonoBehaviour
         }
 
         lineRenderer.SetPosition(4, markedPositions[0]); // Close the loop
-        transform.SetParent(redirectionManager.trackedSpace.transform);
+        //transform.SetParent(redirectionManager.trackedSpace.transform);
     }
 
     private void CalculateAndLogArea()
